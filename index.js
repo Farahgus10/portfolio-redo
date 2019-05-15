8c8ea881384ccf13ffb6d955b64738c3d0db7b0a
Farah Student <farah.gus@hotmail.com>
2019-05-14 22:05:25 -0400

    responsive header page and about section working

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ const STORE = {
 function generateMainPageHTML(){
   return ` 
     <div id="main-page">
-
-        <nav role="navigation" class="contact">
-            <button class="js-contact-button" type="button">Contact me</button>
-          </nav>
         
         <section class="header">
             <header role="banner">
+
               <div class="name">
                 <h1>Farah Gustafson</h1>
               </div>
@@ -23,15 +20,43 @@ function generateMainPageHTML(){
                 <h2>Full stack developer</h2>
               </div>
 
-              <div class="contact
+              <div class="contact">
+                <div class="phone">
+                  <p class="contact-title">phone</p>
+                  <p class="contact-info">(301) 542 4486</p>
+                </div>
+                <div class="email">
+                  <p class="contact-title">email</p>
+                  <p class="contact-info">farah.gustafson@gmail.com</p>
+                </div>
+                <div class="location">
+                  <p class="contact-title">location:</p>
+                  <p class="contact-info">Washington, D.C.</p>
+                </div>
+                <div class="find-me">
+                  <p class="contact-title">find me</p>
+                  <a class="contact-info" href="https://github.com/Farahgus10">Git</a>
+                  <a class="contact-info" href="https://www.linkedin.com/in/farah-gustafson/">li</a>
+                </div>
+              </div>
+
             </header>
         </section>
 
-        <section class="About-me">
+        <section class="about-me">
             <div class="about">
-                <div class="about-box">About</div>
-                <div class="skills-box">Skills</div>
-                <div class="bio-box">Bio</div>
+                <div class="about-box"><h1>About</h1></div>
+                <div class="bio-box">
+                  <h3>Bio</h3>
+                  <p class="bio-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                  <p class="bio-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="skills-box">
+                  <h3>Skills</h3>
+                  <p class="skills-text">JavaScript, HTML5, CSS3</p>
+                  <p class="skills-text">React, jQuery</p>
+                  <p class="skills-text">Node, Express, PostgreSQL</p>
+                </div>
             </div>
         </section>
 
