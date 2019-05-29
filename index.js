d3dc33ef059593ae9f8fac6dd8e1a640b3aa6efd
Farah Student <farah.gus@hotmail.com>
2019-05-29 16:59:16 -0400

    typewriter name

--- index.js
+++ index.js
@@ -44,40 +44,40 @@ function generateMainPageHTML(){
         </section>
 
         <section class="about-me">
-            <div class="about">
-                <div class="about-box"><h1>About</h1></div>
-                <div class="bio-box">
-                  <h3>Bio</h3>
-                  <p class="bio-text">I'm a full-stack developer who values collaborative work, open minds, and a desire to keep learning above all else.</p>
-                  <p class="bio-text">Besides coding, a typical week consists of exploring the great outdoors with my dog, learning new cooking skills, or curled up with a good book. </p>
-                  <p class="bio-text">If you're interested in working with me, or just want to say hello, feel free to contact me through phone, email, or Linkedin.</p>
-                </div>
-                <div class="skills-box">
-                  <h3>Skills</h3>
-                  <p class="skills-text">JavaScript, HTML5, CSS3</p>
-                  <p class="skills-text">React, jQuery</p>
-                  <p class="skills-text">Node, Express, PostgreSQL</p>
-                </div>
+          <div class="about">
+            <div class="about-box"><h1>About</h1></div>
+            <div class="bio-box">
+              <h3>Bio</h3>
+              <p class="bio-text">I'm a full-stack developer who values collaborative work, open minds, and a desire to keep learning above all else.</p>
+              <p class="bio-text">Besides coding, a typical week consists of exploring the great outdoors with my dog, learning new cooking skills, or curled up with a good book. </p>
+              <p class="bio-text">If you're interested in working with me, or just want to say hello, feel free to contact me through phone, email, or Linkedin.</p>
             </div>
+            <div class="skills-box">
+              <h3>Skills</h3>
+              <p class="skills-text">JavaScript, HTML5, CSS3</p>
+              <p class="skills-text">React, jQuery</p>
+              <p class="skills-text">Node, Express, PostgreSQL</p>
+            </div>
+          </div>
         </section>
 
         <section class="projects-section">
-            <div class="projects">
-                <div class="project-main"><h1>Projects</h1></div>
-                
-                <a class="project-link" href="https://farah-sustainachoice-app.now.sh">
-                  <div class="project-picture"></div>
-                </a>
-                
-                <div class="project-title">SustainAChoice</div>
-                <div class="project-description">An app that allows users in the Washington, D.C. area to find sustainably-sourced food in their area.</br>
-                  <p class="tech-stack">Technical stack: React, Node, PostgreSQL, Express, CSS</p>
+          <div class="projects">
+            <div class="project-main"><h1>Projects</h1></div>
+            
+            <a class="project-link" href="https://farah-sustainachoice-app.now.sh">
+              <div class="project-picture"></div>
+            </a>
+            
+            <div class="project-title">SustainAChoice</div>
+            <div class="project-description">An app that allows users in the Washington, D.C. area to find sustainably-sourced food in their area.</br>
+              <p class="tech-stack">Technical stack: React, Node, PostgreSQL, Express, CSS</p>
 
-                  <a href="">Client-side repo</a></br>
-                  <a href="">Server-side repo</a></br>
-                </div>
-                
+              <a href="">Client-side repo</a></br>
+              <a href="">Server-side repo</a></br>
             </div>
+              
+          </div>
         </section>
         
     </div>`;
