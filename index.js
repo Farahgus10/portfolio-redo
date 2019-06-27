e45203e5c79764ecb34630a84df170c0985e3d8c
Farah Student <farah.gus@hotmail.com>
2019-06-27 16:35:31 -0400

    css

--- index.js
+++ index.js
@@ -15,37 +15,42 @@ function generateMainPageHTML(){
         <div id="contact-top">Contact</div>
       </nav>
         
-        <section class="header" id="contact">
-            <header role="banner">
-
+        <section id="contact">
+          <div class="header">
+            
+            
               <div class="name">
                 <h1>Farah Gustafson</h1>
               </div>
               <div class="occupation">
                 <h2>Full stack developer</h2>
               </div>
+           
 
+              <div class="contact-wrapper">
               <div class="contact">
                 <div class="phone">
-                  <p class="contact-title">phone</p>
-                  <p class="contact-info">(301) 542 4486</p>
+                  <p class="contact-title title">phone</p>
+                  <p class="contact-info info">(301) 542 4486</p>
                 </div>
                 <div class="email">
-                  <p class="contact-title">email</p>
-                  <p class="contact-info">farah.gustafson@gmail.com</p>
+                  <p class="contact-title title">email</p>
+                  <p class="contact-info info">farah.gustafson@gmail.com</p>
                 </div>
                 <div class="location">
-                  <p class="contact-title">location:</p>
-                  <p class="contact-info">Washington, D.C.</p>
+                  <p class="contact-title title">location:</p>
+                  <p class="contact-info info">Washington, D.C.</p>
                 </div>
                 <div class="find-me">
-                  <p class="contact-title">find me</p>
-                  <a class="contact-info" href="https://github.com/Farahgus10">GitHub</a>
+                  <p class="contact-title title">find me</p>
+                  <a class="contact-info info" href="https://github.com/Farahgus10">GitHub</a>
                   <a class="contact-info" href="https://www.linkedin.com/in/farah-gustafson/">Linkedin</a>
                 </div>
               </div>
-
-            </header>
+              </div>
+              
+            
+          </div>
         </section>
 
         <section class="about-me">
@@ -70,21 +75,44 @@ function generateMainPageHTML(){
 
         <section class="projects-section">
           <div class="projects" id="projects">
+            
             <div class="project-main"><h1>Projects</h1></div>
-            
-            <div class="project-picture">
-            <a class="project-link" href="https://farah-sustainachoice-app.now.sh"></a>
-              </div>
-            
-            
-            <div class="project-title"> SustainAChoice </div>
-            <div class="project-description"> An app that allows users in the Washington, D.C. area to find sustainably-sourced food in their area. </br>
-              <p class="tech-stack"> Technical stack: React, Node, PostgreSQL, Express, CSS </p>
 
-              <a href=""> Client-side repo </a></br>
-              <a href=""> Server-side repo </a></br>
-            </div>
-              
+            <section>
+            <div class="project1">
+                <div class="project-picture">
+                  <a class="project-link" href="https://farah-sustainachoice-app.now.sh"></a>
+                </div>
+                
+                <div class="project-info">
+                  <div class="project-title"> SustainAChoice </div>
+                  <div class="project-description"> An app that allows users in the Washington, D.C. area to find sustainably-sourced food in their area. </br>
+                    <p class="tech-stack"> Technical stack: React, Node, PostgreSQL, Express, HTML, CSS </p>
+                    <a href="https://github.com/Farahgus10/SustainaChoice-client"> Client-side repo </a></br>
+                    <a href="https://github.com/Farahgus10/SustainaChoice-server"> Server-side repo </a></br>
+                  </div>
+                </div>
+              </div>
+            <section>
+
+            <section>
+              <div class="project-left">
+                <div class="project2-picture"> 
+                  <a class="project-left-link" href="https://farah-quonn-spaced-repetition-app.now.sh/register"></a>
+                </div>
+
+                <div class="project2-info">
+                  <div class="project-left-title"> Spaced Repetition </div>
+                  <div class="project-left-description"> Learn new French words using the spaced repetition technique. </br>
+                    <p class="tech-stack"> Technical stack: React, Node, PostgreSQL, Express, HTML, CSS </p>
+                    <a href="https://github.com/thinkful-ei-bee/Farah-Quonn-Spaced-Repetition-Client"> Client-side repo </a></br>
+                    <a href="https://github.com/thinkful-ei-bee/Farah-Quonn-Spaced-Repetition-Api"> Server-side repo </a></br>
+                  </div>
+                </div>
+              </div>
+
+            </section>
+
           </div>
         </section>
         
@@ -114,7 +142,7 @@ function renderPortfolio(){
       console.log(x);
       $('html, body').animate({
         scrollTop: $("#" + section).offset().top
-      }, 200);
+      }, 350);
     });
   }
 
